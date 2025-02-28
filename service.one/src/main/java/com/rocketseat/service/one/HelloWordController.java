ad7d8d936f7aa745543c22776a58c0c1dd2627b0
erickknsilva <erick.nsilva100@gmail.com>
2025-02-28 00:46:05 -0300

    Configuração do Eureka Server e Eureka client, utilizando um centralizador de configuração do Github, para configuração do aplicativo

---
+++ service.one/src/main/java/com/rocketseat/service/one/HelloWordController.java
@@ -0,0 +1,18 @@
+package com.rocketseat.service.one;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWordController {
+
+    @Value("${message: Hello default}")
+    private String message;
+
+    @GetMapping("/welcome")
+    public String helloWord() {
+        return "<h2>" + this.message + "</h2>";
+    }
+}
